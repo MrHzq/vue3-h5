559d30a85555e3763f9314e6442d64d7f197eee8
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-08-15 10:42:10 +0800

    feat(Lang): 初始化 vue-i18n

---
+++ src/business/lang/index.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n'
+import langs from './lang.json'
+
+const locale = 'en'
+
+const i18n = createI18n({
+  locale, // 当前显示的语言
+  fallbackLocale: 'en', // 兜底语言环境
+  globalInjection: true, // 是否为每个组件注入全局属性和函数。如果设置为true，则以$为前缀的属性和方法将注入Vue Component，eg: $t|$i18n。default:true
+  legacy: false, // set `false`, 切换到 Composition API 模式
+  messages: langs // 语言数据
+})
+
+export default i18n
